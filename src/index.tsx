2addf65ce2778f2e7f1863626dd18690ed1305f6
Zeshan Amjad <zeshan@zesh.me>
2019-12-13 21:09:00 +0000

    Add basic CSS reset

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import ReactDOM from "react-dom";
 
 const globalStyles = css`
   body {
+    margin: 0;
+    padding: 0;
     font-family: "Cerebri Sans", -apple-system, BlinkMacSystemFont, "Segoe UI",
       "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
       "Helvetica Neue", sans-serif;
