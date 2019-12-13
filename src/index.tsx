86b1ee66f0b247d7a933202834b507f8f2aa02d3
Zeshan Amjad <zeshan@zesh.me>
2019-12-13 21:03:38 +0000

    Reformat global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,18 @@ import { css, Global } from "@emotion/core";
 import OverviewPage from "pages/OverviewPage/OverviewPage";
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+
+const globalStyles = css`
+  body {
+    font-family: "Cerebri Sans", -apple-system, BlinkMacSystemFont, "Segoe UI",
+      "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
+  }
+`;
 
 ReactDOM.render(
   <Fragment>
-    <Global
-      styles={css`
-        body {
-          font-family: "Cerebri Sans", -apple-system, BlinkMacSystemFont,
-            "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans",
-            "Droid Sans", "Helvetica Neue", sans-serif;
-        }
-      `}
-    />
+    <Global styles={globalStyles} />
     <OverviewPage />
   </Fragment>,
   document.getElementById("root")
