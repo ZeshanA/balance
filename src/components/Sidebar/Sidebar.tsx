2f10a56806e77370475ef5053c94400e94f75035
Zeshan Amjad <zeshan@zesh.me>
2019-12-13 21:38:43 +0000

    Add FontAwesome icon support

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
+import { faWeight } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import colors from "styles/colors";
 import fonts from "styles/fonts";
@@ -12,17 +14,24 @@ const sidebarStyles = css`
 
 const logoStyles = css`
   width: 100%;
+  border-bottom: solid 1px ${colors.neutrals.grey[8]};
   padding: 20px;
   text-align: center;
   color: ${colors.primary.purple[5]};
-  font-weight: 600;
+  font-weight: 400;
   font-size: ${fonts.sizes[4]};
   box-sizing: border-box;
+
+  svg {
+    margin-right: 2px;
+  }
 `;
 
 const Sidebar: React.SFC = () => (
   <div css={sidebarStyles}>
-    <div css={logoStyles}>Balance</div>
+    <div css={logoStyles}>
+      <FontAwesomeIcon icon={faWeight} /> Balance
+    </div>
   </div>
 );
 
