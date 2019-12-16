fc82b2d19b3aa5934a71814e47736d2581290f91
Zeshan Amjad <zeshan@zesh.me>
2019-12-16 22:10:15 +0000

    Add headings to Delta Statistics

--- src/components/Statistic/Statistic.tsx
+++ src/components/Statistic/Statistic.tsx
@@ -1,8 +1,9 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 import { IconDefinition } from "@fortawesome/pro-regular-svg-icons";
-import React from "react";
+import React, { Fragment } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import _ from "lodash";
 
 export interface StatisticProps {
   children: React.ReactNode;
@@ -10,12 +11,22 @@ export interface StatisticProps {
   unit: string;
 }
 
+export const StatisticValue: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => <Fragment>{children}</Fragment>;
+
 const Statistic: React.FC<StatisticProps> = props => {
   const { icon, children, unit } = props;
+  const [valueChild, otherChildren]: React.ReactNode[][] = _.partition(
+    React.Children.toArray(children),
+    (child: React.ReactElement) =>
+      (child as React.ReactElement)?.type === StatisticValue
+  );
   return (
     <span {...props}>
+      {otherChildren}
       {icon && <FontAwesomeIcon icon={icon} />}
-      <span css={{ marginLeft: "0.25em" }}>{children}</span>
+      <span css={{ marginLeft: "0.25em" }}>{valueChild}</span>
       <span
         css={{
           marginLeft: "0.25em",
--- src/pages/OverviewPage/DeltaCard.tsx
+++ src/pages/OverviewPage/DeltaCard.tsx
@@ -1,32 +1,46 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { css, jsx } from "@emotion/core";
 import { faArrowDown, faHistory } from "@fortawesome/pro-regular-svg-icons";
 import Card from "components/Card/Card";
 import React from "react";
 import CardHeading from "../../components/Card/CardHeading";
-import Statistic from "../../components/Statistic/Statistic";
+import Statistic, {
+  StatisticValue
+} from "../../components/Statistic/Statistic";
 import fonts from "../../styles/fonts";
 
 /* ================================================================================================================== */
 /* Styles
 /* ================================================================================================================== */
-interface DeltaStatisticProps {
-  children: React.ReactNode;
-}
-const DeltaStatistic: React.FC<DeltaStatisticProps> = props => (
-  <Statistic
-    {...props}
-    css={{ fontSize: fonts.sizes[8], display: "block" }}
-    unit="kg"
-    icon={faArrowDown}
-  >
-    {props.children}
-  </Statistic>
-);
 
 /* ================================================================================================================== */
 /* Component
 /* ================================================================================================================== */
+interface DeltaStatisticProps {
+  children: React.ReactNode;
+  title?: string | React.ReactNode;
+}
+const DeltaStatistic: React.FC<DeltaStatisticProps> = props => (
+  <section>
+    <Statistic
+      {...props}
+      css={{ fontSize: fonts.sizes[8], display: "block" }}
+      unit="kg"
+      icon={faArrowDown}
+    >
+      {props.children}
+    </Statistic>
+  </section>
+);
+
+const statisticHeadingStyles = css`
+  font-size: ${fonts.sizes[1]};
+  font-weight: 100;
+  strong {
+    font-weight: 500;
+  }
+`;
+
 const DeltaCard: React.FC = props => (
   <Card {...props}>
     <CardHeading icon={faHistory}>Change Over Time</CardHeading>
@@ -39,10 +53,30 @@ const DeltaCard: React.FC = props => (
         rowGap: "20px"
       }}
     >
-      <DeltaStatistic>2.9</DeltaStatistic>
-      <DeltaStatistic>0.6</DeltaStatistic>
-      <DeltaStatistic>2.3</DeltaStatistic>
-      <DeltaStatistic>2.5</DeltaStatistic>
+      <DeltaStatistic>
+        <h3 css={statisticHeadingStyles}>
+          From Your <strong>Start Date</strong>
+        </h3>
+        <StatisticValue>2.9</StatisticValue>
+      </DeltaStatistic>
+      <DeltaStatistic>
+        <h3 css={statisticHeadingStyles}>
+          This <strong>Week</strong>
+        </h3>
+        <StatisticValue>2.9</StatisticValue>
+      </DeltaStatistic>
+      <DeltaStatistic>
+        <h3 css={statisticHeadingStyles}>
+          This <strong>Month</strong>
+        </h3>
+        <StatisticValue>0.6</StatisticValue>
+      </DeltaStatistic>
+      <DeltaStatistic>
+        <h3 css={statisticHeadingStyles}>
+          This <strong>Year</strong>
+        </h3>
+        <StatisticValue>2.3</StatisticValue>
+      </DeltaStatistic>
     </div>
   </Card>
 );
